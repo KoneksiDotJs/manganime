525b46f4bb446f976f88d1358b7509c10ca071fc
Atala <azleidszhea225@gmail.com>
2024-12-30 13:57:52 +0700

    modified(tailwind.config.ts)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,19 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+const config: Config = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '10rem',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require('daisyui')],
+};
+export default config;
