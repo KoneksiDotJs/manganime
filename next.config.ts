e71b7cf4870533fe51311a6cd03a9341cc77bede
Atala <azleidszhea225@gmail.com>
2024-12-30 13:56:36 +0700

    modified(next.config.ts)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'otakudesu.cloud',
+        port: '',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+module.exports = withPWA(nextConfig);
